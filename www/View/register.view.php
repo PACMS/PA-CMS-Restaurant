13de120e7005b5daccd3a10e3a525bc690491e4f
Jeremie Barriere <barriere.jeremie@gmail.com>
2022-02-02 15:41:48 +0100

    :lipstick: [feature] Framework css #2

--- www/View/register.view.php
+++ www/View/register.view.php
@@ -1,4 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="./public/dist/main.css">
+</head>
+
 <h1>S'inscrire</h1>
+<button class="button">TEST CSS</button>
 
 
 <?php $this->includePartial("form", $user->getCompleteRegisterForm()); ?>
