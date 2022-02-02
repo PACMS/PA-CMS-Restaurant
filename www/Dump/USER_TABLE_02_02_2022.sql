c6bfe29d18bc489c383d477a75f4a3c30e5ce969
LETHUMIER Romain <romain.lethumier@gmail.com>
2022-02-02 15:27:04 +0100

    :sparkles: Add Dump folder

---
+++ www/Dump/USER_TABLE_02_02_2022.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : database
+-- Généré le : mer. 02 fév. 2022 à 14:21
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `mvcdocker2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pacm_user`
+--
+
+CREATE TABLE `pacm_user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(320) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `firstname` varchar(25) DEFAULT NULL,
+  `lastname` text,
+  `status` tinyint(4) NOT NULL,
+  `token` char(255) DEFAULT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `pacm_user`
+--
+ALTER TABLE `pacm_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `pacm_user`
+--
+ALTER TABLE `pacm_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
