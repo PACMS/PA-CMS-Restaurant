97cfbf2fdeab8f120a216da135701514c533ad6e
RAMIS Lucas <lucas.ramis7@gmail.com>
2022-02-04 14:34:07 +0100

    Ajout routes et variables

--- www/conf.inc.php
+++ www/conf.inc.php
@@ -7,3 +7,8 @@ define("DBHOST", "database");
 define("DBNAME", "mvcdocker2");
 define("DBPORT", "3306");
 define("DBPREFIXE", "xldf_");
+
+define("MHOST", "smtp.gmail.com");
+define("MUSERNAME", "pa.cms.test@gmail.com");
+define("MPASSWORD", "!3qJHP?yz6C!DxDH");
+define("MPORT", "465");
