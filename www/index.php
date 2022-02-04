f4f35271161d4a1b7d76eadacb0a6b7fae9a684a
RAMIS Lucas <lucas.ramis7@gmail.com>
2022-02-04 14:45:33 +0100

    index

--- www/index.php
+++ www/index.php
@@ -4,9 +4,9 @@ namespace App;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../phpmailer/src/Exception.php';
-require '../phpmailer/src/PHPMailer.php';
-require '..//phpmailer/src/SMTP.php';
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
 
 require "conf.inc.php";
 
