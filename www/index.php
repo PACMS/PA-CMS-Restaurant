d59b17ed285b589078864765730a587de11ce0ee
ThibautSembeni <tsembeni@myges.fr>
2022-02-19 17:48:34 +0100

    :fire: remove phpmail require in index.php

--- www/index.php
+++ www/index.php
@@ -1,13 +1,6 @@
 <?php
 namespace App;
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require '../phpmailer/src/Exception.php';
-require '../phpmailer/src/PHPMailer.php';
-require '..//phpmailer/src/SMTP.php';
-
 require "conf.inc.php";
 
 function myAutoloader( $class )
