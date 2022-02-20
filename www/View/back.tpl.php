655199ddb5b00eb35da5e41785b97fdcb39d3c35
Vivian Ruhlmann <vivian.fr@free.fr>
2022-02-20 19:29:33 +0100

    :lipstick: (css): Add navbar and flex PA-84
    Ajout de la navbar sur la page Dashboard
    Ajout des flexbox

--- www/View/back.tpl.php
+++ www/View/back.tpl.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <title>Template du back</title>
     <meta name="description" content="ceci est une super page">
+    <link rel="stylesheet" type="text/css" href="../public/dist/main.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <script src="../public/dist/main.js"></script>
 </head>
 <body>
 
--- www/View/dashboard.view.php
+++ www/View/dashboard.view.php
@@ -1,4 +1,13 @@
-<h1>Tableau de bord</h1>
-Welcome <?= $firstname?> <?= $lastname ?>
+<!-- <h1>Bienvenue dans votre Dashboard</h1> -->
+<!-- Welcome <?= $firstname ?> <?= $lastname ?> -->
 
 
+<div class="flex flex-column navbar align-items-center">
+    <img class="companyProfileImg" src="https://images.unsplash.com/photo-1554469384-e58fac16e23a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTgwOTN8MHwxfHNlYXJjaHwzfHxidWlsZGluZ3xlbnwwfHx8fDE2NDUzODA4MTQ&ixlib=rb-1.2.1&q=80&w=1080" alt="Avatar">
+    <h2>Nom Entreprise</h2>
+    <a href="#"><button><i class="far fa-user-circle"></i>Profil</button></a>
+    <a href="#"><button><i class="far fa-edit"></i>Thèmes</button></a>
+    <a href="#"><button><i class="far fa-chart-bar"></i>Statistiques</button></a>
+    <a href="#"><button><i class="far fa-lemon"></i>Restaurants</button></a>
+    <a href="#"><button><i class="far fa-list-alt"></i>Utilisateurs</button></a>
+</div>
