2a7c047af0c085a05256b20d6169fb9915267406
Vivian Ruhlmann <vivian.fr@free.fr>
2022-02-21 15:12:33 +0100

    :lipstick: (css): Add topBar on Dashboard PA-84
    Ajout de la barre de navigation du haut
    Ajout de la page profil

--- www/View/profile.view.php
+++ www/View/profile.view.php
@@ -1,6 +1,3 @@
-<!-- <h1>Bienvenue dans votre Dashboard</h1> -->
-<!-- Welcome <?= $firstname ?> <?= $lastname ?> -->
-
 <div class="flex pageDashboard">
     <div class="flex flex-column navbar justify-content-between align-items-stretch flex-wrap">
         <div class="flex flex-column align-items-center">
@@ -8,7 +5,7 @@
                 <img class="companyProfileImg" src="https://images.unsplash.com/photo-1554469384-e58fac16e23a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTgwOTN8MHwxfHNlYXJjaHwzfHxidWlsZGluZ3xlbnwwfHx8fDE2NDUzODA4MTQ&ixlib=rb-1.2.1&q=80&w=1080" alt="Avatar">
                 <h2>Nom Entreprise</h2>
             </a>
-            <a href="profile"><button><i class="far fa-user-circle"></i>Profil</button></a>
+            <a href="#"><button><i class="far fa-user-circle"></i>Profil</button></a>
             <a href="#"><button><i class="far fa-edit"></i>Thèmes</button></a>
             <a href="#"><button><i class="far fa-chart-bar"></i>Statistiques</button></a>
             <a href="#"><button><i class="far fa-lemon"></i>Restaurants</button></a>
@@ -19,17 +16,12 @@
     <div class="flex flex-column secondPart">
         <div class="flex justify-content-between topBar align-items-center">
             <div class="flex align-items-center">
-                <img src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTgwOTN8MHwxfHNlYXJjaHw2fHxhdmF0YXJ8ZW58MHx8fHwxNjQ1NDQ1MjIx&ixlib=rb-1.2.1&q=80&w=1080" alt="Avatar">
-                <h1>Bienvenue sur votre Dashboard</h1>
+                <h1>Profil</h1>
             </div>
-            <div id="profileDiv">
-                <a href="profile">
+            <div>
+                <a href="#">
                     <p>Jean Pierre<i class="fas fa-user"></i></p>
                 </a>
-                <button>
-                    <i class="far fa-moon"></i>
-                    <i class="fas fa-toggle-off"></i>
-                </button>
             </div>
         </div>
 
---
+++ www/public/src/js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#navbarButton').click(function() {
+        $(this).children('.far').toggleClass('rotated');
+    });
+})
