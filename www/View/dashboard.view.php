b10c7f9d722cb159fdc03701076a8d3835169e7e
Jérémie Barrière <barriere.jeremie@gmail.com>
2022-02-27 17:17:06 +0100

    :lipstick: topbar css

--- www/View/dashboard.view.php
+++ www/View/dashboard.view.php
@@ -16,19 +16,21 @@
         </div>
         <p class="align-self-end"><button id="navbarButton"><i class="far fa-arrow-alt-circle-left"></i>Réduire</button></p>
     </div>
-    <div class="flex flex-column secondPart">
-        <div class="flex justify-content-between topBar align-items-center">
+    <div class="flex flex-column secondPart" >
+        <div class="flex justify-content-between topBar align-items-center" style="padding-right: 35px;" >
             <div class="flex align-items-center">
                 <img src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTgwOTN8MHwxfHNlYXJjaHw2fHxhdmF0YXJ8ZW58MHx8fHwxNjQ1NDQ1MjIx&ixlib=rb-1.2.1&q=80&w=1080" alt="Avatar">
                 <h1>Bienvenue sur votre Dashboard</h1>
             </div>
-            <div id="profileDiv">
+            <div class="flex align-items-center gap-20" >
                 <a href="profile">
-                    <p>Jean Pierre<i class="fas fa-user"></i></p>
+                    <p class="m-0">Jean Pierre<i class="fas fa-user"></i></p>
                 </a>
-                <button>
-                    <i class="far fa-moon"></i>
-                    <i class="fas fa-toggle-off"></i>
+                <button style="background: none; border: none">
+                    <i class="far fa-moon" ></i>
+                </button>
+                <button style="background: none; border: none">
+                    <i class="fas fa-toggle-off" ></i>
                 </button>
             </div>
         </div>
@@ -48,7 +50,7 @@
                 </div>
                 <div class="flex flex-column justify-content-end">
                     <div class="flex gap-7" >
-                        <strong class="data m-0" >140</strong><p class="m-0 percentData--success" >+7,00%</p>
+                        <strong class="data m-0" >140</strong><p class="m-0 percentData--success" >+7,00    %</p>
                     </div>
                     <small>abonnés à la newsletter</small>
                 </div>
@@ -57,7 +59,7 @@
                 <a class="voir-plus-button" href="">Voir plus</a>
             </div>
         </div>
-        <section class="container-preview-cards">
+        <!-- <section class="container-preview-cards">
             <article class="card">
                 <figure>
                     <img src="https://marketplace.canva.com/EAEPN913uPs/1/0/1131w/canva-vert-et-or-case-bordure-g%C3%A9om%C3%A9trique-floral-mariage-menu-DI7xnk_h_VU.jpg" alt="Benefit 3">
@@ -118,6 +120,6 @@
                     <h2>pour Pizza Gogo</h2>
                 </footer>
             </article>
-        </section>
+        </section> -->
     </div>
 </div>
