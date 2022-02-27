677ead7a987ace886a40fe046b1b679e27637cc1
RAMIS Lucas <lucas.ramis7@gmail.com>
2022-02-27 23:00:55 +0100

    PA-86/ Ajout :lipstick:

--- www/View/profile.view.php
+++ www/View/profile.view.php
@@ -32,15 +32,19 @@
         <section class="flex flex-column">
 
             <div class="flex flex-row align-items-center">
-                <img src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTgwOTN8MHwxfHNlYXJjaHw2fHxhdmF0YXJ8ZW58MHx8fHwxNjQ1NDQ1MjIx&ixlib=rb-1.2.1&q=80&w=1080" alt="Avatar">
+                <div class="profileImg">
+                    <img src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTgwOTN8MHwxfHNlYXJjaHw2fHxhdmF0YXJ8ZW58MHx8fHwxNjQ1NDQ1MjIx&ixlib=rb-1.2.1&q=80&w=1080" alt="Avatar">
+                    <i class="fas fa-folder-open"></i>
+                </div>
+
                 <div class="flex-column">
                     <p class="title">Jean-Pierre</p>
                     <p class="role top-32">Patron</p>
                 </div>
 
-                <a href="#">
+                <button id="editProfile">
                     <i class="far fa-pen "></i>
-                </a>
+                </button>
 
             </div>
             <div class="container">
@@ -49,24 +53,27 @@
                     <!-- Prenom -->
                     <div class="flex flex-column">
                         <label class="greytext" for="firstname">Prénom</label>
-                        <input type="text" id="firstname" name="firstname" value="Jean-Pierre">
+                        <input type="text" id="firstname" name="firstname" value="Jean-Pierre" disabled>
                     </div>
 
                     <!-- Nom de famille -->
                     <div class="flex flex-column">
                         <label class="greytext" for="lastname">Nom de famille</label>
-                        <input type="text" id="lastname" name="lastname" value="Jean-Pierre">
+                        <input type="text" id="lastname" name="lastname" value="Jean-Pierre" disabled>
                     </div>
 
                 </div>
                 <div class="flex flex-column w-full">
                 <!-- Adresse mail -->
-                <label class="greytext mt-12" for="email">Adresse mail</label>
-                <input type="email" id="email" name="email" value="JeanPierreDelasoul@myspace.fr">
+                <label class="greytext mt-8" for="email">Adresse mail</label>
+                <input type="email" id="email" name="email" value="JeanPierreDelasoul@myspace.fr" disabled>
 
                 <!-- Numéro de telephone-->
-                <label class="greytext mt-12" for="phoneNumber">Numero de téléphone</label>
-                <input type="tel" id="phoneNumber" name="phoneNumber" value="01 01 01 01 01">
+                <label class="greytext mt-8" for="phoneNumber">Numero de téléphone</label>
+                <input type="tel" id="phoneNumber" name="phoneNumber" value="01 01 01 01 01" disabled>
+                </div>
+                <div id="sectionButton" class="flex flex-row mt-8 justify-content-end w-full">
+
                 </div>
             </div>
         </section>
--- www/public/dist/main.js
+++ www/public/dist/main.js
@@ -4044,4 +4044,25 @@ $(document).ready(function () {
   $('#navbarButton').click(function () {
     $(this).children('.far').toggleClass('rotated');
   });
+  $('#editProfile').on("click", function (event) {
+    $('.container').css("margin-top", "0px");
+    $("input").attr("disabled", false);
+    var labelOld = $("<label class='greytext mt-8' id='labelOld' for='labelOld'>Ancien mot de passe</label>\n");
+    $('input#email').after(labelOld);
+    var inputOld = $("<input name='passwordOld' id='passwordOld' type='password'>");
+    $('label#labelOld').after(inputOld);
+    var labelNew = $("<label class='greytext mt-8' id='labelNew' for='labelNew'>Nouveau mot de passe</label>\n");
+    $('input#passwordOld').after(labelNew);
+    var inputNew = $("<input name='passwordNew' id='passwordNew' type='password'>");
+    $('label#labelNew').after(inputNew);
+    var labelConfirm = $("<label class='greytext mt-8' id='labelConfirm' for='labelConfirm'>Confirmation du mot de passe</label>\n");
+    $('input#passwordNew').after(labelConfirm);
+    var inputConfirm = $("<input name='confirmNewPassowrd' id='confirmNewPassowrd' type='password'>");
+    $('label#labelConfirm').after(inputConfirm);
+    var cancelButton = $("<button class='btn btn-cancel mr-4' id='btncancel'>Annuler </button>");
+    $('div#sectionButton').append(cancelButton);
+    var submitButton = $("<button class='btn btn-submit' id='btncancel'>Confirmer </button>");
+    $('button#btncancel').after(submitButton);
+    $(this).off(event);
+  });
 });
