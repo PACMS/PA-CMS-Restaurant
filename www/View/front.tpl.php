2fc715bda3b52dcd0ede0511cd0fdd356bd5273a
ThibautSBN <tsembeni@myges.fr>
2022-03-23 23:29:05 +0100

    :bug: solve css problem with html responsive

--- www/View/front.tpl.php
+++ www/View/front.tpl.php
@@ -12,6 +12,7 @@ session_start(
     <meta charset="UTF-8">
     <title><?= $title??"Titre par défaut" ?></title>
     <meta name="description" content="ceci est une super page">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../public/dist/main.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <script src="../public/dist/main.js"></script>
