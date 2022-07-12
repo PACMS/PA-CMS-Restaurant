86b25f940727f723560838c50d288efc15c3b740
RAMIS Lucas <lucas@heliop.com>
2022-07-12 23:36:03 +0200

    bug sur css pour le modal

---
+++ www/Core/CommentObserver.php
@@ -0,0 +1,11 @@
+<?php
+
+class CommentObserver implements \SplObserver
+{
+    public function update(\SplSubject $subject): void
+    {
+        if ($subject->state < 3) {
+            echo "ConcreteObserverA: Reacted to the event.\n";
+        }
+    }
+}
