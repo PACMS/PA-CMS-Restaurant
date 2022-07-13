2927c959ec3e4eb0288c20a6937f58bd7dbded8d
RAMIS Lucas <lucas@heliop.com>
2022-07-13 15:36:54 +0200

    bug color qrcode + login listener

--- www/Core/LoggerObserver.class.php
+++ www/Core/LoggerObserver.class.php
@@ -4,10 +4,17 @@ namespace App\Core;
 
 use App\Core\Auth;
 use App\Core\AuthObserver;
+use Cassandra\Cluster\Builder;
 
 class LoggerObserver implements AuthObserver {
 
     function update(Auth $auth, string $event) {
-        die("Auth logger: $event\n");
+        if ($event = 'user:login'){
+            $request = new MysqlBuilder();
+            $request->insert("activitylog", ['user_id' => $_SESSION['user']['id'], 'state' => 'login'])
+                ->fetchClass("activitylog")
+                ->execute();
+        }
+        
     }
 }
