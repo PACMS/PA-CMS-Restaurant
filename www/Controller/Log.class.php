20aa567909a7c14d596598694ef8111f951b08b5
Vivian Ruhlmann <vivian.fr@free.fr>
2022-07-13 17:17:31 +0200

    :construction:(Logs): Fetch all logs

--- www/Controller/Log.class.php
+++ www/Controller/Log.class.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Core\MysqlBuilder;
 use App\Core\View;
 
 class Log
@@ -10,6 +11,13 @@ class Log
     public function index()
     {
         session_start();
+
+        $builder = new MysqlBuilder();
+        $logs = $builder->select('activitylog al', ['al.id', 'state', 'firstname', 'lastname', 'email', 'al.created_at'])
+            ->rightJoin('user u', 'u.id', 'al.user_id')
+            ->fetchClass("activitylog")
+            ->fetchAll();
+
         $view = new View('logs', 'back');
         $view->assign('title', 'Logs');
        // $view->assign('logs', $log);
---
+++ www/Model/ActivityLog.class.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Model;
+
+use App\Core\Sql;
+
+class activitylog extends Sql
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+}
