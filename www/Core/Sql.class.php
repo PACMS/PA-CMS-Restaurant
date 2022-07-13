a03187389920b124828f9cc6b81f50f3774a01e5
Vivian Ruhlmann <vivian.fr@free.fr>
2022-07-14 01:03:09 +0200

    :bug:(Login): Fix login when user not validated

--- www/Core/Sql.class.php
+++ www/Core/Sql.class.php
@@ -270,6 +270,10 @@ abstract class Sql
             header("Location: /login");
         } else {
             if (password_verify($_POST['password'], $userVerify['password'])) {
+                if ($userVerify['status'] !== 1) {
+                    header("Location: /login?error=loginstatus");
+                    die();
+                }
                 session_start();
                 $_SESSION['user']['id'] = $userVerify['id'];
                 $_SESSION['user']['email'] = $userVerify['email'];
