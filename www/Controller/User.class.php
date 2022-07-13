88d57400f32b43436f3c5418a35ba8bbd8d514fa
Vivian Ruhlmann <vivian.fr@free.fr>
2022-07-13 16:37:00 +0200

    :sparkles:(Observer): Add login event with google connect

--- www/Controller/User.class.php
+++ www/Controller/User.class.php
@@ -205,6 +205,12 @@ class User
         $_SESSION['user']['lastname'] = $userInfos['lastname'];
         $_SESSION['user']['role'] = $userInfos['role'];
 
+        $auth = new Auth();
+        $logger = new LoggerObserver();
+
+        $auth->attach($logger);
+        $auth->loginEvent();
+
         switch($userInfos['role']) {
             case 'user':
                 header('Location: /');
