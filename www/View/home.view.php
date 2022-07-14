81931e8fb389248083dab61841ccc7be2c4a83d3
Sembeni Thibaut <tsembeni@myges.fr>
2022-07-14 18:52:17 +0200

    :hammer: custome ux, in homepage

--- www/View/home.view.php
+++ www/View/home.view.php
@@ -1,3 +1,15 @@
 <main>
     <h1>Bienvenue</h1>
+
+
+    <?php if (!empty($pages)) : ?>
+        <h3>Aucun restaurant n'est encore disponible !</h3>
+    <?php elseif (!empty($pages)) : ?>
+        <h3>Voici la liste des restaurants disponibles :</h3>
+        <ul>
+            <?php foreach ($pages as $page): ?>
+                <li><a href="<?= $page->getUrl() ?>"><?= $page->name ?> (<?= $page->getTitle() ?>)</a></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
 </main>
