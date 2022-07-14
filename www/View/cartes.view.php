4d9238a8882821c6419b4c9544c8d22d1fed6ca8
ThibautSBN <90249990+ThibautSembeni@users.noreply.github.com>
2022-07-14 16:48:34 +0200

    Update cartes.view.php
    Change <?php echo to <?=

--- www/View/cartes.view.php
+++ www/View/cartes.view.php
@@ -5,11 +5,11 @@
     <?php $this->includePartial("topBar", ["title" => "Cartes"]); ?>
         <div class="flex flex-column ">
             <?php if (file_exists('public/assets/img/qrcode/qrcode' . $restaurant->getId() . '.svg')){ ?>
-                <a id="" href="/public/assets/img/qrcode/qrcode<?php echo $restaurant->getId() ?>.svg"  class="flex justify-content-center btn btn-submit w-64 mr-3  " download>Télécharger le Qrcode de la carte</a>
+                <a id="" href="/public/assets/img/qrcode/qrcode<?= $restaurant->getId() ?>.svg"  class="flex justify-content-center btn btn-submit w-64 mr-3  " download>Télécharger le Qrcode de la carte</a>
             <?php }?>
             <div class="flex flex-row align-items-center">
                 <?php if (file_exists('public/assets/img/qrcode/qrcode' . $restaurant->getId() . '.svg')){ ?>
-                    <img class="w-48 " src="/public/assets/img/qrcode/qrcode<?php echo $restaurant->getId() ?>.svg" alt="qrcode<?php echo $restaurant->getId() ?>">
+                    <img class="w-48 " src="/public/assets/img/qrcode/qrcode<?= $restaurant->getId() ?>.svg" alt="qrcode<?= $restaurant->getId() ?>">
                     <a id="" href="#open-modalQrcode"  class="flex justify-content-center btn btn-submit w-48 ">Modifier le Qrcode</a>
                 <?php }else echo '<p class="mr-3"> Pas de page carte créé </p>' ?>
 
