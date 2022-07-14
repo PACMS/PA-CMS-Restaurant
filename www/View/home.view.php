74a8c8e2a2ee421db3195a33d9b8ae43b0be193a
Jérémie Barrière <barriere.jeremie@gmail.com>
2022-07-14 23:54:08 +0200

    :fix: navfront partial prod ?

--- www/View/home.view.php
+++ www/View/home.view.php
@@ -1,20 +1,22 @@
 <div class="topnav">
-    <?php if (!empty($_SESSION['user'])) : ?>
-    <li class="right"><a href="/logout">Déconnexion</a></li>
-    <?php else : ?>
-    <li class="right"><a href="/login">Connexion</a></li>
-    <?php endif; ?>
+    <ul>
+    </ul>
+    <div>
+        <?php if (!empty($_SESSION['user'])) : ?>
+            <a href="/logout">Déconnexion</a>
+        <?php else : ?>
+            <a href="/login">Connexion</a>
+        <?php endif; ?>
+    </div>
 </div>
 <main>
     <h1>Bienvenue</h1>
-
-        
     <?php if (empty($pages)) : ?>
         <h3>Aucun restaurant n'est encore disponible !</h3>
     <?php elseif (!empty($pages)) : ?>
         <h3>Voici la liste des restaurants disponibles :</h3>
         <ul>
-            <?php foreach ($pages as $page): ?>
+            <?php foreach ($pages as $page) : ?>
                 <li><a href="<?= $page->getUrl() ?>"><?= $page->name ?> (<?= $page->getTitle() ?>)</a></li>
             <?php endforeach; ?>
         </ul>
