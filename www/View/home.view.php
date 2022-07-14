cdc370b38d730a9f217eaac843bc6a0f5a0cadf8
Jérémie Barrière <barriere.jeremie@gmail.com>
2022-07-15 01:05:42 +0200

    Revert ":fix: navfront partial prod ?"
    This reverts commit 74a8c8e2a2ee421db3195a33d9b8ae43b0be193a.

--- www/View/home.view.php
+++ www/View/home.view.php
@@ -1,22 +1,20 @@
 <div class="topnav">
-    <ul>
-    </ul>
-    <div>
-        <?php if (!empty($_SESSION['user'])) : ?>
-            <a href="/logout">Déconnexion</a>
-        <?php else : ?>
-            <a href="/login">Connexion</a>
-        <?php endif; ?>
-    </div>
+    <?php if (!empty($_SESSION['user'])) : ?>
+    <li class="right"><a href="/logout">Déconnexion</a></li>
+    <?php else : ?>
+    <li class="right"><a href="/login">Connexion</a></li>
+    <?php endif; ?>
 </div>
 <main>
     <h1>Bienvenue</h1>
+
+        
     <?php if (empty($pages)) : ?>
         <h3>Aucun restaurant n'est encore disponible !</h3>
     <?php elseif (!empty($pages)) : ?>
         <h3>Voici la liste des restaurants disponibles :</h3>
         <ul>
-            <?php foreach ($pages as $page) : ?>
+            <?php foreach ($pages as $page): ?>
                 <li><a href="<?= $page->getUrl() ?>"><?= $page->name ?> (<?= $page->getTitle() ?>)</a></li>
             <?php endforeach; ?>
         </ul>
