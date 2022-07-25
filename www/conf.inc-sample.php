0ff7bffdf08c1440314b4df8604cf6165deb23e1
Jérémie Barrière <barriere.jeremie@gmail.com>
2022-07-25 11:49:31 +0200

    Update conf.inc-sample.php

--- www/conf.inc-sample.php
+++ www/conf.inc-sample.php
@@ -9,23 +9,21 @@ define('DBPORT', 'port_de_la_bdd');
 define('DBNAME', 'nom_de_la_bdd');
 define('DBPREFIXE', 'prefixe_de_la_bdd');
 
-define('MHOST', 'smtp.gmail.com');
-define('MUSERNAME', 'pa.cms.test@gmail.com');
-define('MPASSWORD', 'nsxktiyegnnvmlie');
-define('MPORT', '465');
+define('MHOST', 'host_mail');
+define('MUSERNAME', 'username_mail');
+define('MPASSWORD', 'password_mail');
+define('MPORT', 'port_mail');
 
-
-
-define('PUBLIC_KEY_GOOGLE', '592098083518-1ts92tmugsj1kn5b8f64f5vdti2gf0gl.apps.googleusercontent.com');
-define('PRIVATE_KEY_GOOGLE', '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');
+define('PUBLIC_KEY_GOOGLE', 'public_key_google');
+define('PRIVATE_KEY_GOOGLE', 'private_key_google');
 define('URL_API_OAUTH_GOOGLE', 'https://oauth2.googleapis.com/token');
 define('URL_API_INFO_GOOGLE', 'https://openidconnect.googleapis.com/v1/userinfo');
 define('REDIRECT_URI_GOOGLE', 'https://localhost/googleConnect');
 
-define('PUBLIC_KEY_FACEBOOK', '1412626972532730');
-define('PRIVATE_KEY_FACEBOOK', '03af7457fa2dd6afdb9a1a2bc788ac4f');
+define('PUBLIC_KEY_FACEBOOK', 'public_key_facebook');
+define('PRIVATE_KEY_FACEBOOK', 'private_key_google');
 define('URL_API_OAUTH_FACEBOOK', 'https://graph.facebook.com/v13.0/oauth/access_token');
 define('URL_API_INFO_FACEBOOK', 'https://graph.facebook.com/v13.0/me?fields=email,last_name,first_name');
 define('REDIRECT_URI_FACEBOOK', 'https://localhost/facebookConnect');
 
-define('FONTS_KEY_API', 'AIzaSyBo1F6LeW8iTlOoK9rauItIwa1X79ct6cw');
+define('FONTS_KEY_API', 'font_key'); 
